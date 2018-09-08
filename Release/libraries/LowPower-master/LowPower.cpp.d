8638068ae3ba8b0afc62b942603af059c5a80de4
Paolo Lupo <paolo.lupo.1985@db.com>
2018-09-08 22:14:45 +0200

    Deleted Release folder

--- Release/libraries/LowPower-master/LowPower.cpp.d
+++
@@ -1,5 +0,0 @@
-libraries/LowPower-master/LowPower.cpp.o: \
- /home/paolo/Arduino/libraries/LowPower-master/LowPower.cpp \
- /home/paolo/Arduino/libraries/LowPower-master/LowPower.h
-
-/home/paolo/Arduino/libraries/LowPower-master/LowPower.h:
